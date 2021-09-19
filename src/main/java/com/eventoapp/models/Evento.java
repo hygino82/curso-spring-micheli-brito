668597800d00070ba9c4b78cb00acfe1ac63c8f1
Adroaldo <hygino@msn.com>
2021-09-19 11:04:50 -0300

    Criando e configurando um banco de dados MySQL (sem classe de configuração)

--- src/main/java/com/eventoapp/models/Evento.java
+++ src/main/java/com/eventoapp/models/Evento.java
@@ -1,11 +1,27 @@
 package com.eventoapp.models;
 
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
+
+@Entity
 @Getter
 @Setter
-public class Evento {
+public class Evento implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
+	
 	private String nome;
 	private String local;
 	private String data;
