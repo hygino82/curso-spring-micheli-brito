c640896ade78416433ec491e93da41cc0dcb5599
Adroaldo <hygino@msn.com>
2021-09-19 10:16:38 -0300

    Criando as camadas MVC

---
+++ src/main/java/com/eventoapp/models/Evento.java
@@ -0,0 +1,13 @@
+package com.eventoapp.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Evento {
+	private String nome;
+	private String local;
+	private String data;
+	private String horario;
+}
